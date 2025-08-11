39b5462b74f27d0606d3ea7542471c7a48a9e76f
chemutai254 <nancy.chemutai254@gmail.com>
2025-08-11 16:09:29 +0300

    Python Program

---
+++ README.md
@@ -0,0 +1,6 @@
+# Python program to perform calculations
+
+# Tasks
+* Create a simple Python program that asks the user to input two numbers and a mathematical operation (addition, subtraction, multiplication, or division).
+* Perform the operation based on the user's input and print the result.
+
---
+++ calculator.py
@@ -0,0 +1,9 @@
+# Input two numbers
+num1 = input("Enter first number: ")
+num2 = input("Enter second number: ")
+
+# Print outputs
+print(f"Addition: {float(num1) + float(num2)}")
+print(f"Subtraction: {float(num1) - float(num2)}")
+print(f"Multiplication: {float(num1) * float(num2)}")
+print(f"Division: {float(num1) / float(num2)}")
